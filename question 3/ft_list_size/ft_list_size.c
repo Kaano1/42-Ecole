0250c423c65d950c7ac39f1edf0b7c2867cffe73
farukdll <sefoni8668@gmail.com>
2022-12-15 00:13:38 +0300

    exam2

--- question 3/ft_list_size/ft_list_size.c
+++ question 3/ft_list_size/ft_list_size.c
@@ -1,13 +1,3 @@
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-
-typedef struct s_list
-{
-    struct s_list *next;
-    void *data;
-} t_list;
-
 int ft_list_size(t_list *begin_list)
 {
     if (begin_list)
