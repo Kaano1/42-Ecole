fd5ad570e46e52b8a45ed14cf2a3b64ed8325322
farukdll <sefoni8668@gmail.com>
2022-12-19 10:58:21 +0300

    exam2

--- question 1/rotone/rotone.c
+++ question 1/rotone/rotone.c
@@ -10,7 +10,7 @@ int main(int ac, char **av)
 		{
 			if ((av[1][i] >= 'a' && av[1][i] <= 'y') || (av[1][i] >= 'A' && av[1][i] <= 'Y'))
 				av[1][i] += 1;
-			else if (av[1][i] == 'z' && av[1][i] == 'Z')
+			else if (av[1][i] == 'z' || av[1][i] == 'Z')
 				av[1][i] -= 25;
 			write(1, &av[1][i], 1);
 			i++;
