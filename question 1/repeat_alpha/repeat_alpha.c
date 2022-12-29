58cca5b586095b318ce41c0052921df900b14064
farukdll <sefoni8668@gmail.com>
2022-12-29 04:08:32 +0300

    exam2

--- question 1/repeat_alpha/repeat_alpha.c
+++ question 1/repeat_alpha/repeat_alpha.c
@@ -3,21 +3,25 @@
 int main(int ac, char **av)
 {
     int i = 0;
-    int k = 1;
-
+    int k = 0;
     if (ac == 2)
     {
         while (av[1][i])
         {
-            if (av[1][i] >= 'A' && av[1][i] <= 'Z')
-                k = av[1][i] - 64;
             if (av[1][i] >= 'a' && av[1][i] <= 'z')
                 k = av[1][i] - 96;
-            while (k >= 1)
+            else if (av[1][i] >= 'A' && av[1][i] <= 'Z')
+                k = av[1][i] - 64;
+            if (k != 0)
             {
-                write(1, &av[1][i], 1);
-                k--;
+                while (k > 0)
+                {
+                    write(1, &av[1][i], 1);
+                    k--;
+                }
             }
+            else
+                write(1, &av[1][i], 1);
             i++;
         }
     }
