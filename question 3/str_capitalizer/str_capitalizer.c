637546119e0db451e5b1eafeda3a81c6a81e41ef
farukdll <sefoni8668@gmail.com>
2022-12-30 16:55:50 +0300

    exam2

--- question 3/str_capitalizer/str_capitalizer.c
+++ question 3/str_capitalizer/str_capitalizer.c
@@ -4,15 +4,15 @@ void str_capital(char *str)
 {
     int i = 0;
 
-    if (str[i] >= 'a' && 'z' >= str[i])
+    if (str[i] >= 'a' && str[i] <= 'z')
         str[i] -= 32;
     write(1, &str[i], 1);
 
     while (str[++i])
     {
-        if (str[i] >= 'A' && 'Z' >= str[i])
+        if (str[i] >= 'A' && str[i] <= 'Z')
             str[i] += 32;
-        if ((str[i] >= 'a' && 'z' >= str[i]) && (str[i - 1] == ' ' || str[i - 1] == '\t'))
+        if ((str[i] >= 'a' && str[i] <= 'z') && (str[i - 1] == ' ' || str[i - 1] == '\t'))
             str[i] -= 32;
         write(1, &str[i], 1);
     }
