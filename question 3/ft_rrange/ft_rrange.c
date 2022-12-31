253e28999d51723be0f6f54474bc0b0bffd2efbc
Faruk PEKTAS <97880185+farukdll@users.noreply.github.com>
2022-12-31 17:38:41 +0300

    Update ft_rrange.c

--- question 3/ft_rrange/ft_rrange.c
+++ question 3/ft_rrange/ft_rrange.c
@@ -7,7 +7,7 @@ int *ft_rrange(int start, int end)
     int n = start > end ? start - end + 1 : end - start + 1;
     int dir = start < end ? 1 : -1;
 
-    range = (int *)malloc(sizeof(int) * n);
+    range = malloc(sizeof(int) * n);
     while (n--)
     {
         range[n] = start;
