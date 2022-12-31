d37da8af282360b8a12a5cac112f80721b41481d
Faruk PEKTAS <97880185+farukdll@users.noreply.github.com>
2022-12-31 16:24:54 +0300

    Update ft_swap.c

--- question 1/ft_swap/ft_swap.c
+++ question 1/ft_swap/ft_swap.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include <stdio.h>
 
 void    ft_swap(int *a, int *b)
 {
@@ -8,3 +8,17 @@ void    ft_swap(int *a, int *b)
     *a = *b;
     *b = tmp;
 }
+
+/*
+int main()
+{
+    int a;
+    int b;
+
+    a = 24; // a value
+    b = 42; // b value
+    printf("Before --> a = %d    b = %d\n", a, b);
+    ft_swap(&a, &b);
+    printf("After  --> a = %d    b = %d\n", a, b);
+}
+*/
