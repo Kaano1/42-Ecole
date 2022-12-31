4be3b668a052ce51dce94260cbbccb774a3f9844
Faruk PEKTAS <97880185+farukdll@users.noreply.github.com>
2022-12-31 16:38:28 +0300

    Update ft_strcmp.c

--- question 2/ft_strcmp/ft_strcmp.c
+++ question 2/ft_strcmp/ft_strcmp.c
@@ -8,3 +8,12 @@ int ft_strcmp(char *s1, char *s2)
         i++;
     return (s1[i] - s2[i]);
 }
+
+/*
+int main()
+{
+    char k1[] = "abc";
+    char k2[] = "def";
+    printf("%d\n", ft_strcmp(k1, k2));
+}
+*/
