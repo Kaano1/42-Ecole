4b5a09c055cf9ef1679a56314ddb1d3492ce9c50
Faruk PEKTAS <97880185+farukdll@users.noreply.github.com>
2023-01-25 22:49:54 +0300

    Update max.c

--- question 2/max/max.c
+++ question 2/max/max.c
@@ -2,19 +2,20 @@
 
 int max(int *tab, unsigned int len)
 {
-    if (tab == NULL || len == 0)
-        return (0);
-
-    int i = 0;
-    int res = tab[i];
-
-    while (i < len)
-    {
-        if (res < tab[i])
-            res = tab[i];
-        i++;
-    }
-    return (res);
+	unsigned int i = 0;
+	int	max;
+    
+	if (len == i)
+		return (0);
+    
+	max = tab[0];
+	while (len > 0 && i < len)
+	{
+		if (tab[i] >= max)
+			max = tab[i];
+		i++;
+	}
+	return (max);
 }
 /*
 int main()
